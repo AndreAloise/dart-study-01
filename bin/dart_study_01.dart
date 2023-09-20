c45d2d2153bd85efc53fe974bc8d2c6b76318d08
AndreAloise <andre.aloise.77@gmail.com>
2023-09-20 01:38:23 -0300

    Criando método auxiliar para print da idade.

--- bin/dart_study_01.dart
+++ bin/dart_study_01.dart
@@ -1,9 +1,25 @@
 void main(List<String> arguments)
 {
-  printText('Hello world!');
+  int age = 20;
+  printAge(age);
+
+  String toHexadecimal = intToHexadecimal(age);
+  printText('Now in Hexadecimal: 0x$toHexadecimal');
 }
 
 void printText(String text)
 {
   print(text);
+}
+
+void printAge(int age)
+{
+  String text = 'My age is: $age years.';
+  printText(text);
+}
+
+String intToHexadecimal(int value)
+{
+  String convertedValue = value.toRadixString(16);
+  return convertedValue;
 }
