ca1486f7cfb81303659894d3514a4642eade607a
AndreAloise <andre.aloise.77@gmail.com>
2023-09-25 20:53:28 -0300

    Adicionando propriedades e método que informa se está boa para consumo.

--- bin/Orange.dart
+++ bin/Orange.dart
@@ -1,1 +1,19 @@
-class Orange {}
+class Orange {
+  String name = '';
+  double weight = 0.0;
+  String firstColor = '';
+  String secondColor = '';
+  String flavor = '';
+  int harvestDays = 0;
+  late final int daysToBeRiped;
+  late final int daysToBeRotten;
+
+  Orange(int daysTbRipped, int daysTbRotten){
+    daysToBeRiped = daysTbRipped;
+    daysToBeRotten = daysTbRotten;
+  }
+
+  bool isRippedToConsumption(){
+    return ((daysToBeRiped <= harvestDays) && (harvestDays < daysToBeRotten));
+  }
+}
