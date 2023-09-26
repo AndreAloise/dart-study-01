237824b73ad2d2de2ee5fa2cb82598d3a678ee5b
AndreAloise <andre.aloise.77@gmail.com>
2023-09-25 21:42:11 -0300

    Criando vínculo de herança entre orange e fruit

--- bin/orange.dart
+++ bin/orange.dart
@@ -1,27 +1,9 @@
-class Orange {
-  String name = '';
-  double weight = 0.0;
-  String firstColor = '';
-  String secondColor = '';
-  String flavor = '';
-  int harvestDays = 0;
-  late final int daysToBeRiped;
-  late final int daysToBeRotten;
+import 'abstract_fruit.dart';
 
-  Orange(int daysTbRipped, int daysTbRotten){
+class Orange extends Fruit{
+
+  Orange(int daysTbRipped, int daysTbRotten) {
     daysToBeRiped = daysTbRipped;
     daysToBeRotten = daysTbRotten;
-  }
-
-  bool isRippedToConsumption(){
-    return ((daysToBeRiped <= harvestDays) && (harvestDays < daysToBeRotten));
-  }
-
-  @override
-  String toString(){
-    return 'The fruit $name has $weight kg and was harvested $harvestDays day ago.\n'
-        'As we can see, it has a predominant shade of $firstColor,'
-        'with some patches of $secondColor.\n'
-        'When you taste is, is has a $flavor flavor.';
   }
 }
