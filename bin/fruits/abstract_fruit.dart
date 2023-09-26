dc95e71b3e6f8bd262d60e128b167b7e000040d7
AndreAloise <andre.aloise.77@gmail.com>
2023-09-25 22:40:40 -0300

    Melhorando as mensagens

--- bin/fruits/abstract_fruit.dart
+++ bin/fruits/abstract_fruit.dart
@@ -16,15 +16,14 @@ abstract class Fruit{
   String toString(){
     bool isGoodToConsume = isRippedToConsumption();
 
-    String textWithMultipleColors = 'The fruit $name has $weight kg and was harvested $harvestDays day(s) ago.\n'
+    String textWithMultipleColors = '\nThe fruit $name has $weight kg and was harvested $harvestDays day(s) ago.\n'
         'As we can see, originally it has a predominant shade of $firstColor,'
         'with some patches of $secondColor.\n';
 
-    String textWithOneColor = 'The fruit $name has $weight kg and was harvested $harvestDays day(s) ago.\n'
-        'As we can see, originally it has the color $firstColor.\n'
-        'When you taste is, is has a $flavor flavor.';
+    String textWithOneColor = '\nThe fruit $name has $weight kg and was harvested $harvestDays day(s) ago.\n'
+        'As we can see, originally it has the color $firstColor.\n';
 
-    String flavorText = 'When you taste is, it has a $flavor flavor.';
+    String flavorText = 'When you taste it, it has a $flavor flavor.';
     String rottenText = 'You should not taste it because it is Rotten.';
 
     String lastPhrase = isGoodToConsume ? flavorText : rottenText;
