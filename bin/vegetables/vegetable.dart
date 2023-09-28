c4318ac2a7aa0c32452d5047fc95a6b27e34a09d
AndreAloise <andre.aloise.77@gmail.com>
2023-09-28 16:07:31 -0300

    Criando propriedades para os vegetais

---
+++ bin/vegetables/vegetable.dart
@@ -0,0 +1,9 @@
+class Vegetable{
+  late String name;
+  late double weight;
+  late String firstColor;
+  late String secondColor;
+  late String flavor;
+  late int harvestDays;
+  late final int daysToBeRotten;
+}
