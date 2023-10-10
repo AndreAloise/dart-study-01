d2600abe3f41718152e176d59b1306d83aa516a1
AndreAloise <andre.aloise.77@gmail.com>
2023-10-10 01:32:41 -0300

    Criando classe de um mercado simples que usará assincronísmo.

---
+++ bin/markets/simple_market.dart
@@ -0,0 +1,8 @@
+class SimpleMarket{
+  /*
+  Class to create a simple automatic market, to add Food in a basket according
+  to the options presented to the user, in the terminal, and calculate the
+  total cost of the basket.
+  The interaction with the user must have some asynchronous methods.
+   */
+}
